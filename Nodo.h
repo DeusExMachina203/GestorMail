60a4e64b4718c81961419e5eafcc73c4461d77ec
Joaquín Carbajal Pozzo <joaco.carbajal@hotmail.com>
2023-09-15 11:34:42 -0500

    creo el archivo nodo

---
+++ Nodo.h
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+template<class T>
+class Nodo {
+public:
+	T valor;
+	Nodo<T>* siguiente;
+public:
+	Nodo(T v, Nodo<T>* sig = NULL) {
+		valor = v;
+		siguiente = sig;
+	}
+};
