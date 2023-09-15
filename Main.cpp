445a1fedeb83647b0c5f47333c61345d138dfe35
Joaquín Carbajal Pozzo <joaco.carbajal@hotmail.com>
2023-09-15 11:35:23 -0500

    menu creado y main avanzado

---
+++ Main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include"Menu.h"
+using namespace System;
+using namespace std;
+
+int main() {
+	while (1) {
+		int op1;
+		logo();
+		cin >> op1;
+		if (op1 == 1) {
+			string usu;
+			string pass;
+
+		}
+		else if (op1 == 2) {
+
+		}
+		Console::Clear();
+	}
+
+	return 0;
+}
---
+++ Menu.h
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+
+void logo() {
+	cout << "	 ▄████████   ▄▄▄▄███▄▄▄▄      ▄████████  ▄█   ▄█" << endl;
+	cout << "	███    ███ ▄██▀▀▀███▀▀▀██▄   ███    ███ ███  ███" << endl;
+	cout << "	███    █▀  ███   ███   ███   ███    ███ ███▌ ███" << endl;
+	cout << "  ▄███▄▄▄     ███   ███   ███   ███    ███ ███▌ ███" << endl;
+	cout << " ▀▀███▀▀▀     ███   ███   ███ ▀███████████ ███▌ ███" << endl;
+	cout << "	███        ███   ███   ███   ███    ███ ███  ███" << endl;
+	cout << "	███        ███   ███   ███   ███    ███ ███  ███▌    ▄" << endl;
+	cout << "	███         ▀█   ███   █▀    ███    █▀  █▀   █████▄▄██" << endl;
+	cout << "											     ▀" << endl;
+	cout << "-----------------------------------------------------------" << endl;
+	cout << "						Bienvenido!!" << endl;
+	cout << "Seleccione una opción: " << endl;
+	cout << "1. Iniciar Sesión" << endl;
+	cout << "2. Crear una cuenta" << endl;
+}
+
+void menuPrincipal() {
+	cout << "Elija una de las sguientes opciones:" << endl;
+	cout << "1. Revisar Correos" << endl;
+	cout << "2. Enviar un Correo" << endl;
+	cout << "3. Cerrar Sesion" << endl;
+}
